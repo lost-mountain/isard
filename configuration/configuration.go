3666cac912245319e7dc31afcd62943482151c25
David Calavera <david.calavera@gmail.com>
2017-02-20 09:58:41 +0100

    Start a global configuration structure.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -6,3 +6,25 @@ const (
 	// StagingDirectory is the address where the staging ACME directory is.
 	StagingDirectory = "http://localhost:4000/directory"
 )
+
+// Configuration hold setup
+// information for the service
+// to work.
+type Configuration struct {
+	Domains DomainsConfiguration
+	ACME    struct {
+		DefaultProductionDirectory string
+		DefaultStagingDirectory    string
+	}
+}
+
+// DomainsConfiguration holds setup
+// information to request certificates
+// and validate domains.
+type DomainsConfiguration struct {
+	Ns1APIKey       string
+	HeaderValidator struct {
+		Name  string
+		Value string
+	}
+}
