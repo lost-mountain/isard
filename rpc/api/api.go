8757b7ef85ccf390f7c0fa5fc326dc675c8f2f40
David Calavera <david.calavera@gmail.com>
2017-03-21 19:19:58 -0700

    Implement basic API to create certificates and retrieve them.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- rpc/api/api.go
+++ rpc/api/api.go
@@ -3,8 +3,13 @@ package api
 import (
 	"context"
 
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+
 	"github.com/lost-mountain/isard/account"
+	"github.com/lost-mountain/isard/broker"
 	"github.com/lost-mountain/isard/configuration"
+	"github.com/lost-mountain/isard/domain"
 	"github.com/lost-mountain/isard/rpc"
 	"github.com/lost-mountain/isard/storage"
 )
@@ -14,6 +19,7 @@ import (
 type API struct {
 	configuration *configuration.Configuration
 	bucket        storage.Bucket
+	broker        broker.Broker
 }
 
 // CreateAccount creates a new domain account.
@@ -54,8 +60,29 @@ func (a *API) UpdateAccount(context.Context, *rpc.UpdateAccountRequest) (*rpc.Up
 
 // CreateCertificate starts the process to request a domain certificate.
 // It creates a new domain and negotiates the challenge type.
-func (a *API) CreateCertificate(context.Context, *rpc.CreateCertificateRequest) (*rpc.CreateCertificateResponse, error) {
-	return nil, nil
+func (a *API) CreateCertificate(ctx context.Context, req *rpc.CreateCertificateRequest) (*rpc.CreateCertificateResponse, error) {
+	accID, err := uuid.Parse(req.AccountID)
+	if err != nil {
+		return nil, errors.Wrap(err, "invalid account ID format")
+	}
+	accountToken, err := uuid.Parse(req.AccountToken)
+	if err != nil {
+		return nil, errors.Wrap(err, "invalid account token format")
+	}
+
+	c := &broker.CreateDomainPayload{
+		AccountID:     accID,
+		AccountToken:  accountToken,
+		DomainName:    req.Domain,
+		ChallengeType: req.ChallengeType,
+	}
+
+	m := broker.NewMessage(c)
+	if err := a.broker.Publish(broker.Creation, m); err != nil {
+		return nil, err
+	}
+
+	return &rpc.CreateCertificateResponse{}, nil
 }
 
 // ResolveCertificateChallenge returns the information required to resolve a challenge.
@@ -69,6 +96,33 @@ func (a *API) CheckCertificateState(context.Context, *rpc.CertificateStateReques
 }
 
 // GetCertificate returns the domain certificate once it has been authorized by the CA.
-func (a *API) GetCertificate(context.Context, *rpc.GetCertificateRequest) (*rpc.GetCertificateResponse, error) {
-	return nil, nil
+func (a *API) GetCertificate(ctx context.Context, req *rpc.GetCertificateRequest) (*rpc.GetCertificateResponse, error) {
+	accID, err := uuid.Parse(req.AccountID)
+	if err != nil {
+		return nil, errors.Wrap(err, "invalid account ID format")
+	}
+	accountToken, err := uuid.Parse(req.AccountToken)
+	if err != nil {
+		return nil, errors.Wrap(err, "invalid account token format")
+	}
+
+	acc, err := a.bucket.GetAccount(accID, accountToken)
+	if err != nil {
+		return nil, err
+	}
+
+	d, err := a.bucket.GetDomain(acc.ID, req.Domain)
+	if err != nil {
+		return nil, err
+	}
+
+	if d.State != domain.Issued {
+		return nil, errors.New("the certificate has not been issued yet")
+	}
+
+	return &rpc.GetCertificateResponse{
+		Certificate: string(d.Certificate.Cert),
+		Key:         string(d.Certificate.Key),
+		Chain:       string(d.Certificate.CA),
+	}, nil
 }
